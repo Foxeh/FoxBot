a103d5a7a4b2fdef4100bf14b6b7efd6c1939dff
Ginger <tyler.carper@g2-inc.com>
2016-05-13 16:54:39 -0400

    added rs highscore lookup

--- cmds/runescape.py
+++ cmds/runescape.py
@@ -1,28 +1,49 @@
-import urllib,urllib2,httplib,json
-from xml.etree import ElementTree as etree
+import urllib,urllib2,httplib,json,csv
 
 from interface import Interface
 
 class Runescape(Interface):
     def start(self, *args, **kwargs):
         self.geUri = "http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=" #4798
-        self.hsUri = "http://services.runescape.com/m=hiscore/index_lite.ws?player=" #67
         self.mobUri = "http://services.runescape.com/m=itemdb_rs/bestiary/beastData.json?beastid=" #49
+        self.hsUri = "http://services.runescape.com/m=hiscore/index_lite.ws?player=" #67
+        self.skills = ("Tot", "Att", "Def", "Str", "HP", "Rng", "Pray", "Mage", "Cook", "WC", "Flch", "Fish", "FM", 
+                      "Craft", "Smith", "Mine", "Herb", "Agil", "Thiev", "Slay", "Farm", "RC", "Hunt", "Con", "Sum", 
+                      "Dung", "Div")
         
     def bang_ge(self,data):
         data.conn.msg(data.usernick, self.geUri)
         
     def bang_hs(self,data):
-        data.conn.msg(data.usernick, self.hsUri)
+        response = False
+        try:
+            response = urllib2.urlopen(self.hsUri+data.cmd['parameters'])
+        except urllib2.HTTPError as e:
+            data.conn.msg(data.channel, "User not found.")
+        
+        if response:
+            msg = ""
+            rows = response.read().splitlines()
+            for i,skill in enumerate(self.skills): # enumerate adds a counter (i) to the iterable (self.skills).
+                r = rows[i].split(",") # rank, lvl, xp
+                if r[0] == -1:
+                    continue
+                msg += skill + " " + str(r[1]) + " | "
+            data.conn.msg(data.channel, msg[:-1])
         
     def bang_mob(self,data):
-        
         print ("result: ", urllib2.urlopen(self.mobUri+data.cmd['parameters']).read())
         response = urllib2.urlopen(self.mobUri+data.cmd['parameters']).read()
-        if (response):
+        attrs = (("","name"), (" | LVL ","level"), (" | HP ","lifepoints"), (" | XP ","xp"), (" | Weakness: ","weakness"))
+        if response:
             result = json.loads(response)
-            resp = result['name'] + " | Level " + str(result['level']) + " | Lifepoints: " + str(result['lifepoints']) + " | Weakness: " + result['weakness'] + " | XP: " + result['xp']
-            data.conn.msg(data.channel, resp.encode('utf-8'))
+            msg = ""
+            for label,attr in attrs:
+                if attr in result:
+                    if type(result[attr]) is int:
+                        result[attr] = str(result[attr])
+                    msg += label + result[attr]
+            data.conn.msg(data.channel, msg.encode('utf-8'))
         else:
             data.conn.msg(data.channel, "No Beast with id = "+data.cmd['parameters'])
         
